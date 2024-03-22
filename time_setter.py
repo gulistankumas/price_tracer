7e517ed0da50c961e11c8fffa25069031ed42eb2
Gulistan Kumas <gulistankmas@gmail.com>
2024-03-23 02:07:37 +0300

    updated

--- time_setter.py
+++ time_setter.py
@@ -1,9 +1,9 @@
 import datetime as dt
+from main import is_price_chanced,found_company,url,price_tag
+
 now=dt.datetime.now()
 hour= now.hour
 minute=now.minute
-if hour == '22':
-    if minute=='00':
-        pass
-    #fiyatı kontrol et data da ki değerden farklı bir değerse 
-    #yeni fiyatı 2. basamak olarak yaz ve farkını mail at fiyat her değiştiğinde 1. yi 2., 2.yi yeni fiyat değeri yap
+if hour == 10 or 22 :
+    if minute==00:
+       is_price_chanced(found_company,url,price_tag)
